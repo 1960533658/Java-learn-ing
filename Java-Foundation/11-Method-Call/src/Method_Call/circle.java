709db41abddee50fec5cd52a8681ebc0e94cabb0
1960533658@qq.com <1960533658@qq.com>
2021-05-18 21:45:04 +0800

    完成java方法的定义和调用

--- Java-Foundation/11-Method-Call/src/Method_Call/circle.java
+++ Java-Foundation/11-Method-Call/src/Method_Call/circle.java
@@ -1,4 +1,24 @@
 package Method_Call;
 
 public class circle {
+    // 定义方法的语法
+    /**
+     * pubcli static 返回值的数据类型 方法名(形参) {
+     *      方法体
+     *      return值
+     * }
+     *
+     */
+    public static void main(String[] args) {
+        show();
+        System.out.println(getSum(1,2));
+    }
+    public static void show() {
+        System.out.println("展示圆的面积");
+    }
+
+    public static Number getSum(int num1,int num2) {
+        return num1 + num2;
+    }
+
 }
